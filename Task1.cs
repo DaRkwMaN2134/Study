f9b5c52cebe04d411fa3cfaa4edb837935ad709a
DaRkwMaN2134 <darkman0865@gmail.com>
2026-07-11 16:39:15 +0600

    Добавление 1 задания и обучение работы с git

---
+++ Task1.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число от 0 до 100: ");
+string input = Console.ReadLine(); // Запрос строки
+int number = int.Parse(input);
+
+if(number >= 0 && number <=100)
+{
+    if (number >= 90)
+    {
+        Console.Write("Отлично");
+    }
+    else if (number >= 70)
+    {
+        Console.Write("Хорошо");
+    }
+    else if (number >= 50)
+    {
+        Console.Write("Удовлетворительно");
+    }
+    else if (number >= 0)
+    {
+        Console.Write("Неудовлетворительно");
+    }
+}
+else
+{
+    Console.Write("Число не в диапазоне");
+}
