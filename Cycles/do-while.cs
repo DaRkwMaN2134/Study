8109b46e34a3669c0403fb6cd821e4eb44666490
DaRkwMaN2134 <darkman0865@gmail.com>
2026-07-12 14:53:24 +0600

    Добавление задания + создание ветки

---
+++ Cycles/do-while.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ввелите пароль");
+string n = Console.ReadLine();
+do
+{
+    if (n != "1234")
+    {
+        Console.WriteLine("Пароль неправильный, повторите");
+        n = Console.ReadLine();
+    }
+
+}
+while (n != "1234");
+{
+    Console.WriteLine("Пароль принят");
+}
---
+++ Cycles/for.cs
@@ -0,0 +1,6 @@
+﻿int n = 50;
+for (int i = 0; i < n; i++)
+{
+    if(i % 3 == 0)
+    Console.WriteLine(i);
+}
