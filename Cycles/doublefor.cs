79e24770848217b89a9099a75c2bba23c07faac9
DaRkwMaN2134 <darkman0865@gmail.com>
2026-07-15 14:06:42 +0600

    Восстановление git после отключения электричества при работе с git

---
+++ Cycles/doublefor.cs
@@ -0,0 +1,10 @@
+﻿int tabl = 5;
+int tablmaxvalue = 9;
+for(int i = 1; i <= tabl; i++)
+{
+    Console.WriteLine($"Таблица умножения на {i}");
+    for (int j = 1; j <= tablmaxvalue; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j} ");
+    }
+}
