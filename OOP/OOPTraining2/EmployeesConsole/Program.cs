30aac7a1ee619a49fa2800f2991aeb92ebff1bea
DaRkwMaN2134 <darkman0865@gmail.com>
2026-07-19 13:38:41 +0600

    Работа с ООП, день 2

---
+++ OOP/OOPTraining2/EmployeesConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿List<Employee> employee = new List<Employee>()
+{
+    new Manager("Антон", 20.5, 3),
+    new Manager("Илья", 20.5, 3),
+    new Manager("Владимир", 20.5, 3),
+    new Developer("Александр", 21.0, 2),
+    new Developer("Владислав", 21.0, 3),
+    new Developer("Николай", 21.0, 2)
+};
+
+foreach(Employee worker in employee)
+{
+    worker.PrintInfo();
+    if(worker is IWorkable work)
+    {
+        work.Work();
+    }
+}
---
+++ OOP/OOPTraining2/EmployeesLibrary/Employee.cs
@@ -0,0 +1,56 @@
+﻿public abstract class Employee
+{
+    public abstract string Name { get; set; }
+    public abstract double BaseSalary { get; set; }
+    public abstract double CalculateBonus();
+    public Employee(string name, double basesalary)
+    {
+        Name = name;
+        BaseSalary = basesalary;
+    }
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"Имя: {Name}; Зарплата: {BaseSalary}");
+    }
+}
+
+public class Manager : Employee, IWorkable
+{
+    public override string Name { get; set; }
+    public override double BaseSalary { get; set; }
+    public override double CalculateBonus() => BaseSalary * 1.20;
+    public int TeamSize;
+    public Manager(string name, double basesalary, int teamsize) : base(name, basesalary)
+    {
+        TeamSize = teamsize;
+    }
+    public override void PrintInfo() => Console.WriteLine($"Имя: {Name}; Зарплата: {CalculateBonus()}; Размер команды: {TeamSize}");
+    public void Work()
+    {
+        Console.WriteLine("Manager is managing");
+    }
+}
+
+
+
+public class Developer : Employee, IWorkable
+{
+    public override string Name { get; set; }
+    public override double BaseSalary { get; set; }
+    public int ProjectsCount;
+    public override double CalculateBonus() => BaseSalary * 1.10;
+    public Developer(string name, double baselasary, int projectscount) : base(name, baselasary)
+    {
+        ProjectsCount = projectscount;
+    }
+    public override void PrintInfo() => Console.WriteLine($"Имя: {Name}; Зарплата: {CalculateBonus()}; Количество проектов: {ProjectsCount}");
+    public void Work()
+    {
+        Console.WriteLine("Developer is coding");
+    }
+}
+
+public interface IWorkable
+{
+    void Work();
+}
