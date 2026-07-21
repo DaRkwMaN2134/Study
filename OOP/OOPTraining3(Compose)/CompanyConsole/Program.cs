bd957f572d01bbb52d348c2f0e0f8e71b6f6eec9
DaRkwMaN2134 <darkman0865@gmail.com>
2026-07-21 17:36:31 +0600

    Работа с композицией в ООП

---
+++ OOP/OOPTraining3(Compose)/CompanyConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using CompanyLibrary;
+
+List<IWorkable> list = new List<IWorkable>()
+{
+    new Developer("Антон", 20.5, 3),
+    new Developer("Илья", 20.5, 3),
+    new Developer("Владимир", 20.5, 3),
+    new Manager("Александр", 21.0, 2),
+    new Manager("Владислав", 21.0, 3),
+    new Manager("Николай", 21.0, 2)
+};
+
+foreach(var worker in list)
+{
+    worker.work();
+    if(worker is IPrintable PrintInfo)
+    {
+        PrintInfo.PrintInfo();
+    }
+}
