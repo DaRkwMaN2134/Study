a8b203ca999bb5e42c4cdd1f1068065f21a2d49a
DaRkwMaN2134 <darkman0865@gmail.com>
2026-07-23 18:11:42 +0600

    Работа с делегатами и событиями

---
+++ Delegate+Action/Action/Action.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+
+    static Action<string> GreetingDelegate = name => Console.WriteLine($"Привет, {name}");
+    static void Main()
+    {
+        GreetingDelegate("Илья");
+    }
+}
---
+++ Delegate+Action/Delegate/Delegate.cs
@@ -0,0 +1,21 @@
+﻿delegate void GreetingDelegate(string name);
+
+class Program
+{
+    static void Main()
+    {
+        GreetingDelegate delh = SayHello;
+        GreetingDelegate delg = new GreetingDelegate(SayGoodbye);
+        delh("Илья");
+        delg("Илья");
+    }
+    static void SayHello(string name)
+    {
+        Console.WriteLine($"Привет, {name}");
+    }
+
+    static void SayGoodbye(string name)
+    {
+        Console.WriteLine($"Пока, {name}");
+    }
+}
