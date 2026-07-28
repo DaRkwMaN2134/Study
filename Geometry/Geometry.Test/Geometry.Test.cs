25f10f03460f469108709c5535cc9c42df5ae10f
DaRkwMaN2134 <darkman0865@gmail.com>
2026-07-28 16:05:09 +0600

    Рефакторинг кода + задача на геометрию и ее тесты

---
+++ Geometry/Geometry.Test/Geometry.Test.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+
+namespace Geometry.Test
+{
+    public class Geometry_Test
+    {
+        [Fact]
+        public void AddTest()
+        {
+            var v1 = new Vector2D(2, 2);
+            var v2 = new Vector2D(2, 2);
+            var expected = new Vector2D(4, 4);
+
+            var result = v1.Add(v2);
+
+            Assert.Equal(expected.X, result.X, 5);
+            Assert.Equal(expected.Y, result.Y, 5);
+        }
+
+
+        [Fact]
+        public void SubtractTest()
+        {
+            var v1 = new Vector2D(2, 2);
+            var v2 = new Vector2D(2, 2);
+            var expected = new Vector2D(0, 0);
+
+            var result = v1.Subtract(v2);
+
+            Assert.Equal(expected.X, result.X, 5);
+            Assert.Equal(expected.Y, result.Y, 5);
+        }
+
+
+        [Fact]
+        public void DotTest()
+        {
+            var v1 = new Vector2D(2, 2);
+            var v2 = new Vector2D(1, 1);
+            var expected = new Vector2D(2, 2);
+
+            var result = v1.Dot(v2);
+
+            Assert.Equal(expected.X, result.X);
+            Assert.Equal(expected.Y, result.Y, 5);
+        }
+
+        [Fact]
+        public void LengthTest()
+        {
+            var v = new Vector2D(3, 4);
+            var expected = 5;
+
+            var result = v.Length();
+
+            Assert.Equal(expected, result, 5);
+        }
+
+        [Fact]
+        public void NormalizeTest()
+        {
+            var v = new Vector2D(1, 1);
+            var expected = new Vector2D(0.70710678, 0.70710678);
+
+            var result = v.Normalize();
+
+            Assert.Equal(expected.X, result.X, 5);
+            Assert.Equal(expected.Y, result.Y, 5);
+        }
+    }
+}
---
+++ Geometry/Geometry/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+public class Proramm
+{
+    public static void Main()
+    {
+        Vector2D res = new Vector2D(100, 100).Add(new Vector2D(100, 100));
+        Console.WriteLine($"({res.X}, {res.Y})".ToString());
+    }
+}
+
+
+public class Vector2D
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public Vector2D(double x, double y)
+    {
+        
+        X = x;
+        Y = y;
+    }
+    Vector2D() : this(0, 0) { }
+
+    public Vector2D Add(Vector2D other)
+    {
+        return new Vector2D(this.X + other.X, this.Y + other.Y);
+    }
+
+    public Vector2D Subtract(Vector2D other)
+    {
+        return new Vector2D(this.X - other.X, this.Y - other.Y);
+    }
+
+    public Vector2D Dot(Vector2D other)
+    {
+        return new Vector2D(this.X * other.X, this.Y * other.Y);
+    }
+
+    public double Length()
+    {
+       return Math.Sqrt(((this.X * this.X) + (this.Y * this.Y)));
+    }
+
+    public Vector2D Normalize()
+    {
+        if(Length() == 0)
+        {
+            return new Vector2D(0, 0);
+        }
+        else
+        {
+            return new Vector2D(this.X / Length(), this.Y / Length());
+        }    
+    }
+}
