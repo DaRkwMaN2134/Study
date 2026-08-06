3235241edf3d87d328762cda18fb8aaf86880705
DaRkwMaN2134 <141501991+DaRkwMaN2134@users.noreply.github.com>
2026-08-06 17:16:17 +0600

    Работа над пет-проектом: Этап 6. Заключение

--- SAPR-Calculator/Program/Program.cs
+++ SAPR-Calculator/Program/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Shape_Calculator;
 using System.IO;
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 DateTime startTime = DateTime.Now;
 await Logger.Instance.LogAsync("Программа запущена\n");
 
@@ -8,6 +12,10 @@ string mainfile = "shapes.txt";
 string mainpath = Path.Combine(mainfile);
 
 List<Shape> list = new List<Shape>(){};
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 async Task<string[]> ReadFileAsync()
 {
@@ -18,15 +26,17 @@ async Task<string[]> ReadFileAsync()
     }
     catch (FileNotFoundException)
     {
-        Console.WriteLine("Файл не найден. Создаю файл с примерами...");
+        Console.WriteLine("Файл не найден. Создаю файл с примерами..."); // Заполнение и создание файла(если он отсутствует)
         string[] sampleLines = new string[]
         {
-            "circle 5",
-            "rectangle 4 6",
-            "triangle 3 4 5",
+            "circle 5                ",
+            "rectangle 4 6           ",
+            "triangle 3 4 5          ",
             "circle -2",
             "rectangle 0 5",
-            "triangle 1 1 3"
+            "triangle 1 1 3       ",
+            "               ",
+            "square 1 2"
         };
         await File.WriteAllLinesAsync(mainpath, sampleLines);
         Console.WriteLine("Файл создан. Перезапустите программу.");
@@ -39,16 +49,120 @@ async Task<string[]> ReadFileAsync()
     }
 }
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ (IAreaCalculator Area, IPerimeterCalculator Perimeter) ShapeCalculator(string type)
+{
+    switch(type)
+    {
+        case "circle":
+            var _CircleAreaCalculator = new CircleAreaCalculator();
+            var CirclePerimeterCalculator = new CirclePerimeterCalculator();
+            return (_CircleAreaCalculator, CirclePerimeterCalculator);
+        case "rectangle":
+            var _RectangleAreaCalculator = new RectangleAreaCalculator();
+            var RectanglePerimeterCalculator = new RectanglePerimeterCalculator();
+            return (_RectangleAreaCalculator, RectanglePerimeterCalculator);
+        case "triangle":
+            var _TriangleAreaCalculator = new TriangleAreaCalculator();
+            var TrianglePerimeterCalculator = new TrianglePerimeterCalculator();
+            return(_TriangleAreaCalculator, TrianglePerimeterCalculator);
+        default:
+            return(null, null);
+    }
+        
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+async Task ParseCircleAsync(string type, string[] parts)
+{
+    if (double.TryParse(parts[1], out double radius))
+    {
+        var (area, per) = ShapeCalculator(type);
+        if (radius > 0)
+        {
+            await Logger.Instance.LogAsync($"Создана фигура: {type}, Радиус: {parts[1]}\n");
+            list.Add(ShapeFactory.Create("circle", area, per, [radius]));
+        }
+        else
+        {
+            await Logger.Instance.LogErrorAsync($"Фигура: {type}, Радиус: {parts[1]}\n");
+            Console.WriteLine("Неверные параметры для круга");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Некорректный радиус: {parts[1]}");
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+async Task ParseRectangleAsync(string type, string[] parts)
+{
+    if (double.TryParse(parts[1], out double width) && double.TryParse(parts[2], out double height)) // Перевод текстовых данных в числа с точкой для сравнения
+    {
+        var (area, per) = ShapeCalculator(type);
+        if (width > 0 && height > 0)
+        {
+            await Logger.Instance.LogAsync($"Создана фигура: {type}, Ширина: {parts[1]}, Высота: {parts[2]}\n");
+            list.Add(ShapeFactory.Create("rectangle", area, per, [width, height]));
+        }
+        else
+        {
+            await Logger.Instance.LogErrorAsync($"Фигура: {type}, Ширина: {parts[1]}, Высота: {parts[2]}\n");
+            Console.WriteLine("Неверные параметры для прямоугольника");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Некорректные размеры: {parts[1]}, {parts[2]}");
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+async Task ParseTriangleAsync(string type, string[] parts)
+{
+    if (double.TryParse(parts[1], out double sidea) && double.TryParse(parts[2], out double sideb) && double.TryParse(parts[3], out double sidec))
+    {
+        var (area, per) = ShapeCalculator(type);
+        if (sidea + sideb > sidec && sidea + sidec > sideb && sideb + sidec > sidea)
+        {
+            await Logger.Instance.LogAsync($"Создана фигура: {type}, Стороны: {sidea}, {sideb}, {sidec}\n");
+            list.Add(ShapeFactory.Create("triangle", area, per, [sidea, sideb, sidec]));
+        }
+        else
+        {
+            await Logger.Instance.LogErrorAsync($"Фигура: {type}, Стороны: {sidea}, {sideb}, {sidec}\n");
+            Console.WriteLine("Неравенство треугольника не выполняется");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Некорректные стороны: {parts[1]}, {parts[2]}, {parts[3]}");
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 async Task ShapeDataAsync(string[] lines)
 {
     if (lines == null) return;
-
-    foreach (string line in lines)
+    foreach (string line in lines) 
     {
         if (string.IsNullOrWhiteSpace(line))
             continue;
 
-        string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries); // Разбивка массива на строки
         if (parts.Length == 0)
             continue;
 
@@ -56,31 +170,13 @@ async Task ShapeDataAsync(string[] lines)
 
         switch (type)
         {
-            case "circle":
+            case "circle": // Проверка фигур
                 if (parts.Length < 2)
                 {
                     Console.WriteLine("Недостаточно параметров для круга");
                     break;
                 }
-                if (double.TryParse(parts[1], out double radius))
-                {
-                    var _CircleAreaCalculator = new CircleAreaCalculator();
-                    var CirclePerimeterCalculator = new CirclePerimeterCalculator();
-                    if (radius > 0)
-                    {
-                        await Logger.Instance.LogAsync($"Создана фигура: {type}, Радиус: {parts[1]}\n");
-                        list.Add(ShapeFactory.Create("circle", _CircleAreaCalculator, CirclePerimeterCalculator, [radius]));
-                    }
-                    else
-                    {
-                        await Logger.Instance.LogErrorAsync($"Фигура: {type}, Радиус: {parts[1]}\n");
-                        Console.WriteLine("Неверные параметры для круга");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Некорректный радиус: {parts[1]}");
-                }
+                await ParseCircleAsync(type, parts);
                 break;
 
             case "rectangle":
@@ -89,25 +185,7 @@ async Task ShapeDataAsync(string[] lines)
                     Console.WriteLine("Недостаточно параметров для прямоугольника");
                     break;
                 }
-                if (double.TryParse(parts[1], out double width) && double.TryParse(parts[2], out double height))
-                {
-                    var _RectangleAreaCalculator = new RectangleAreaCalculator();
-                    var RectanglePerimeterCalculator = new RectanglePerimeterCalculator();
-                    if (width > 0 && height > 0)
-                    {
-                        await Logger.Instance.LogAsync($"Создана фигура: {type}, Ширина: {parts[1]}, Высота: {parts[2]}\n");
-                        list.Add(ShapeFactory.Create("rectangle", _RectangleAreaCalculator, RectanglePerimeterCalculator, [width, height]));
-                    }
-                    else
-                    {
-                        await Logger.Instance.LogErrorAsync($"Фигура: {type}, Ширина: {parts[1]}, Высота: {parts[2]}\n");
-                        Console.WriteLine("Неверные параметры для прямоугольника");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Некорректные размеры: {parts[1]}, {parts[2]}");
-                }
+                await ParseRectangleAsync(type, parts);
                 break;
 
             case "triangle":
@@ -116,33 +194,19 @@ async Task ShapeDataAsync(string[] lines)
                     Console.WriteLine("Недостаточно параметров для треугольника");
                     break;
                 }
-                if (double.TryParse(parts[1], out double sidea) && double.TryParse(parts[2], out double sideb) && double.TryParse(parts[3], out double sidec))
-                {
-                    var _TriangleAreaCalculator = new TriangleAreaCalculator();
-                    var TrianglePerimeterCalculator = new TrianglePerimeterCalculator();
-                    if (sidea + sideb > sidec && sidea + sidec > sideb && sideb + sidec > sidea)
-                    {
-                        await Logger.Instance.LogAsync($"Создана фигура: {type}, Стороны: {sidea}, {sideb}, {sidec}\n");
-                        list.Add(ShapeFactory.Create("triangle", _TriangleAreaCalculator, TrianglePerimeterCalculator, [sidea, sideb, sidec]));
-                    }
-                    else
-                    {
-                        await Logger.Instance.LogErrorAsync($"Фигура: {type}, Стороны: {sidea}, {sideb}, {sidec}\n");
-                        Console.WriteLine("Неравенство треугольника не выполняется");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Некорректные стороны: {parts[1]}, {parts[2]}, {parts[3]}");
-                }
+                await ParseTriangleAsync(type, parts);
                 break;
 
             default:
+                await Logger.Instance.LogErrorAsync($"Неизвестный тип фигуры: {type}\n");
                 Console.WriteLine($"Неизвестный тип фигуры: {type}");
                 break;
         }
     }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 string[] lines = await ReadFileAsync();
@@ -170,26 +234,32 @@ TimeSpan duration = endTime - startTime;
 await Logger.Instance.LogAsync($"Время выполнения: {duration.TotalMilliseconds} мс\n");
 
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 public class Logger
 {
     private static Logger _instance;
     private Logger() { }
-    public static Logger Instance
+    public static Logger Instance // Создание синглтона
     {
         get
         {
             if (_instance == null)
-                _instance = new Logger(); // создаётся при первом обращении
+                _instance = new Logger(); // создаётся объект логера при первом обращении
             return _instance;
         }
     }
 
-    public async Task LogAsync(string message)
+
+    public async Task LogAsync(string message) 
     {
         DateTime localTime = DateTime.Now;
         string logfile = "log.txt";
         await File.AppendAllTextAsync(logfile, $" [{localTime}] INFO:{message}");
     }
+
 
     public async Task LogErrorAsync(string message)
     {
