eb295ce2d193fd6102458f7e87931e8605c2584a
DaRkwMaN2134 <141501991+DaRkwMaN2134@users.noreply.github.com>
2026-08-15 16:38:54 +0600

    Тренировка задач с фриланса

---
+++ FreelanceTask/CSV-Read/File-IO/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace File_IO
+{
+    public class FilesWork
+    {
+        string file = "sales.csv";
+        string[] example = [ "Дата; Товар; Количество; Цена",
+                             "2026-08-01; Ноутбук; 2; 50000",
+                             "2026-08-02; Мышь; 5; 1500",
+                             "2026-08-03; Ноутбук; 1; 52000"];
+        public async Task<string[]> BasicFileIO()
+        {
+            try
+            {
+                string[] fileRead = await File.ReadAllLinesAsync(file);
+                return fileRead;
+            }
+            catch (FileNotFoundException)
+            {
+                File.Create("sales.csv").Close();
+                await File.WriteAllLinesAsync(file, example);
+                Console.WriteLine($"Файл не был найден\nПоэтому был создан пример файла по пути '{Path.GetFullPath(file)}'");
+                return null;
+            }
+        }
+
+        public async Task ReportFileIO(string file, List<string> report)
+        {
+            await File.WriteAllLinesAsync(file, report);
+        }
+
+        public async Task ErrorFileIO(string file, List<string> report)
+        {
+            await File.WriteAllLinesAsync(file, report);
+        }
+    }
+}
---
+++ FreelanceTask/CSV-Read/Start/Program.cs
@@ -0,0 +1,84 @@
+﻿using File_IO;
+
+string reportFile = "report.txt";
+List<string> reportData = new List<string> { };
+
+string errorFile = "error.log";
+List<string> errorData = new List<string> { };
+
+
+FilesWork result = new FilesWork();
+string[] data = await result.BasicFileIO();
+
+List<Order> res = new List<Order> {};
+
+
+if (data == null || data.Length == 0)
+{
+    Console.WriteLine("Файл пуст");
+}
+else
+{
+    for (int i = 1; i < data.Length; i++)
+    {
+        var parts = data[i].Split(';');
+        if (parts.Length == 4)
+        {
+            Order currentOrder = new Order();
+            var product = parts[1].Trim();
+            if ((DateTime.TryParse(parts[0].Trim(), out DateTime date) && int.TryParse(parts[2].Trim(), out int qty) && double.TryParse(parts[3].Trim(), out double price) && !string.IsNullOrWhiteSpace(product)) == true)
+            {
+                currentOrder.Date = date;
+                currentOrder.Product = product;
+                currentOrder.Quantity = qty;
+                currentOrder.Price = price;
+                res.Add(currentOrder);
+            }
+            else
+            {
+                errorData.Add($"Ошибка парсинга в строке {i}: {data[i]}");
+                continue;
+            }
+        }
+    }
+
+    var groups = res.GroupBy(o => o.Product)
+                       .Select(g => new
+                       {
+                           Key = g.Key,
+                           TotalSales = g.Sum(o => o.Quantity * o.Price)
+                       })
+                       .ToList();
+    double MaxSales = groups.Max(x => x.TotalSales);
+
+    foreach (var group in groups)
+    {
+        Console.WriteLine($"Товар: {group.Key}, Общая выручка: {group.TotalSales}");
+        reportData.Add($"Товар:{group.Key}, Общая выручка: {group.TotalSales}");
+    }
+    Console.WriteLine($"Максимальная выручка {MaxSales}");
+    reportData.Add($"Максимальная выручка {MaxSales}");
+
+    await result.ReportFileIO(reportFile, reportData);
+    await result.ErrorFileIO(errorFile, errorData);
+}
+
+
+
+
+
+class Order
+{
+    public DateTime Date;
+    public string Product;
+    public int Quantity;
+    public double Price;
+    public Order(){}
+    public Order(DateTime date, string product, int quantity, double price)
+    {
+        Date = date;
+        Product = product;
+        Quantity = quantity;
+        Price = price;
+    }
+}
