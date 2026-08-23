7e69b6505dcd526d130b3b1a7de084ae5663e953
DaRkwMaN2134 <141501991+DaRkwMaN2134@users.noreply.github.com>
2026-08-23 15:56:41 +0600

    Работа с SQL. Первый запрос и сортировка + защита пароля от базы

---
+++ OrdinaryTask/SQL/SQLRequest/RequestStart/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+class Program
+{
+    static async Task Main()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        string connectionString = config.GetConnectionString("Postgres");
+
+        await SelectQuotesAsync(connectionString);
+
+    }
+
+    static async Task SelectQuotesAsync(string connectionString)
+    {
+        var conn = new NpgsqlConnection(connectionString);
+        await conn.OpenAsync();
+       
+        var request = 
+        "SELECT author, COUNT(*) " +
+        "FROM quotes " +
+        "GROUP BY author " +
+        "ORDER BY COUNT(*) DESC";
+
+        var cmd = new NpgsqlCommand(request, conn);
+        var reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+
+            string author = reader.GetString(0);
+            int quotes = reader.GetInt32(1);
+
+            Console.WriteLine($"Автор: {author} — {quotes} цитаты");
+            Console.WriteLine(new string('-', 40));
+        }
+    }
+}
