6e1a90a6d92810ccca8308a2e1124eecb0a69861
DaRkwMaN2134 <141501991+DaRkwMaN2134@users.noreply.github.com>
2026-09-24 15:07:46 +0600

    Завершение проекта. Перехват исключений

--- Portfolio/ReferralBot/BotStart/Bot.Updates.cs
+++ Portfolio/ReferralBot/BotStart/Bot.Updates.cs
@@ -11,72 +11,79 @@ namespace BotStart
     {
         async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            if (update.MyChatMember is { } mychatmember)
+            long chatId = 0;
+            try
             {
-                return;
-            }
-            if (update.CallbackQuery is { } callbackQuery)
-            {
-                await HandleCallbackQueryAsync(callbackQuery, cancellationToken);
-                return;
-            }
-            if (update.Message is not { } message || message.Text is not { } messageText)
-            {
-                return;
-            }
-
-            long chatId = message.Chat.Id;
-            using (var scope = _serviceScopeFactory.CreateAsyncScope())
-            {
-                var botWrite = scope.ServiceProvider.GetRequiredService<IBotWrite>();
-                var user = await botWrite.GetUserAsync(chatId);
-                if (user != null && user.IsBlocked)
+                if (update.MyChatMember is { } mychatmember)
                 {
-                    await botClient.SendMessage(chatId, "Вы заблокированы");
+                    return;
+                }
+                if (update.CallbackQuery is { } callbackQuery)
+                {
+                    await HandleCallbackQueryAsync(callbackQuery, cancellationToken);
+                    return;
+                }
+                if (update.Message is not { } message || message.Text is not { } messageText)
+                {
                     return;
                 }
 
-                if (messageText.StartsWith("/start"))
+                chatId = message.Chat.Id;
+                using (var scope = _serviceScopeFactory.CreateAsyncScope())
                 {
-                    var registration = new UserRegistrationDto
+                    var botWrite = scope.ServiceProvider.GetRequiredService<IBotWrite>();
+                    var user = await botWrite.GetUserAsync(chatId);
+                    if (user != null && user.IsBlocked)
                     {
-                        ChatId = message.Chat.Id,
-                        Username = message.Chat.Username,
-                        FirstName = message.Chat.FirstName
-                    };
-
-                    long refererrId = 0;
-                    await botWrite.CheckOrCreateUserAsync(registration);
-                    if (messageText.Contains("ref_"))
-                    {
-                        string[]? parse = messageText.Split('_');
-                        if (long.TryParse(parse[1], out long result))
-                        {
-                            refererrId = result;
-                        }
-
-                        if (refererrId == chatId)
-                        {
-                            await _botClient.SendMessage(chatId, "Нельзя пригласить себя");
-                            return;
-                        }
-                        else if (refererrId == 0)
-                        {
-                            return;
-                        }
-                        else
-                        {
-                            var referral = await botWrite.GetReferrerAsync(chatId, refererrId);
-                            if (referral.Status == ReferralStatus.Pending)
-                            {
-                                await botWrite.ConfirmReferralAsync(referral);
-                            }
-                        }
-
+                        await botClient.SendMessage(chatId, "Вы заблокированы");
+                        return;
                     }
-                    await ShowMainMenu(chatId);
-                }
 
+                    if (messageText.StartsWith("/start"))
+                    {
+                        var registration = new UserRegistrationDto
+                        {
+                            ChatId = message.Chat.Id,
+                            Username = message.Chat.Username,
+                            FirstName = message.Chat.FirstName
+                        };
+
+                        long refererrId = 0;
+                        await botWrite.CheckOrCreateUserAsync(registration);
+                        if (messageText.Contains("ref_"))
+                        {
+                            string[]? parse = messageText.Split('_');
+                            if (long.TryParse(parse[1], out long result))
+                            {
+                                refererrId = result;
+                            }
+
+                            if (refererrId == chatId)
+                            {
+                                await _botClient.SendMessage(chatId, "Нельзя пригласить себя");
+                                return;
+                            }
+                            else if (refererrId == 0)
+                            {
+                                return;
+                            }
+                            else
+                            {
+                                var referral = await botWrite.GetReferrerAsync(chatId, refererrId);
+                                if (referral.Status == ReferralStatus.Pending)
+                                {
+                                    await botWrite.ConfirmReferralAsync(referral);
+                                }
+                            }
+
+                        }
+                    }
+                }
+                await ShowMainMenu(chatId);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Ошибка при обработке update от {ChatId}", chatId);
             }
         }
     }
